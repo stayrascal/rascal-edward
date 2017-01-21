78d8414b338d09508b015ddcaf643cc7c7660487
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-21 22:05:58 +0800

    add new edward file

---
+++ edward/models/models.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy a np
+import six
+import tensorflow as tf
+import warnings
+
+from edward.util import get_dims, get_session
