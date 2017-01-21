8e2a7abd1966c044599c7eed1fb039f3d3146462
Zhiping Wu <zpwu@thoughtworks.com>
2017-01-21 16:51:07 +0800

    move edward to top folder

--- model/edward/models/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-import numpy a np
-import six
-import tensorflow as tf
-import warnings
-
-from edward.util import get_dims, get_session
